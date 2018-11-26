d796250ab3f250f25d555c1f1c32f72d734c0767
alexis holguin <wholguinmor@uniminuto.edu.co>
2018-11-26 00:12:36 -0500

    definicion de metodos de retiro y recarga de cuenta

--- Models/account.rb
+++ Models/account.rb
@@ -3,16 +3,21 @@
 class Account
   @id
   @balance
-
+  @user_id
+  @mysql_obj
   attr_accessor :balance, :id
 
   def initialize; end
 
   def transfer_money(email); end
 
-  def deposit(amount) 
-    
+  def deposit(amount)
+    @mysql_obj.query("UPDATE `accounts` SET `avaliable` = #{@balance + amount} WHERE id = #{@id}")
+    @mysql_obj.query("INSERT INTO `external_trasactions`(`type`, `user_id`, `amount`) VALUES ('deposit',#{@user_id},#{amount})")
   end
 
-  def withdraw; end
+  def withdraw(amount)
+    @mysql_obj.query("UPDATE `accounts` SET `avaliable` = #{@balance - amount} WHERE id = #{@id}")
+    @mysql_obj.query("INSERT INTO `external_trasactions`(`type`, `user_id`, `amount`) VALUES ('withdraw',#{@user_id},#{amount})")
+  end
 end
