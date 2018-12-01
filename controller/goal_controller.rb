0f40d230a4edefd251cf86c3e177c9c5e8c16f8d
alexis holguin <wholguinmor@uniminuto.edu.co>
2018-12-01 18:39:07 -0500

    controller and form of goal

--- controller/goal_controller.rb
+++ controller/goal_controller.rb
@@ -1,68 +1,37 @@
 class GoalController
-
-    def initialize(user:)
-      @user_input = UserInput.new
-      @user = user
-    end
+  def initialize(user:)
+    @user_input = UserInput.new
+    @user = user
+    @form = GoalForm.new
+  end
 
   def deposit
-    begin
-      print 'Nombre de la meta: '
-    end while !@user_input.validate_user_data_input(field: 'name')
-    name = @user_input.last_input
-    if !@user.search_goal(name).nil?
-      begin
-        print 'ingrese la cantidad de dinero que desea agregar a la meta: '
-      end while !@user_input.validate_amount_input
-      amount = @user_input.last_input.to_i
-      if @user.search_goal(name).deposit(amount, @user.account.available)
-        @user.account.available -= amount
-        puts "\nDinero agregado a la meta #{name} con exito!"
-      else
-        puts "\nLa cantidad a depositar no esta disponible en su cuenta"
-      end
+    loop do
+      data = @form.form_deposit
+      break unless @user.search_goal(data[:name]).nil?
+      puts "\nLa meta #{data[:name]} no existe"
+    end
+    if @user.search_goal(data[:name]).deposit(data[:amount], @user.account.available)
+      @user.account.available -= amount
+      puts "\nDinero agregado a la meta #{data[:name]} con exito!"
     else
-      puts "\nLa meta #{name} no existe"
+      puts "\nLa cantidad a depositar no esta disponible en su cuenta"
     end
   end
 
   def create
-    begin
-      print 'Nombre de la meta: '
-    end while !@user_input.validate_user_data_input(field: 'name')
-    name = @user_input.last_input
-    begin
-      print 'Ingrese la meta de dinero: '
-    end while !@user_input.validate_amount_input
-    expected_amount = @user_input.last_input
-
-    puts 'Por favor ingrese el año, el mes y el dia de vencimiento de la meta en numeros'
-    begin
-      print 'Ingrese el plazo maximo de la meta (año): '
-    end while !@user_input.validate_date_input(type: 'year')
-    year = @user_input.last_input.to_i
-    begin
-      print 'Ingrese el plazo maximo de la meta (mes): '
-    end while !@user_input.validate_date_input(type: 'month')
-    month = @user_input.last_input.to_i
-    begin
-      print 'Ingrese el plazo maximo de la meta (dia): '
-    end while !@user_input.validate_date_input(type: 'day')
-
-    @user.add_goal(name, expected_amount, year, month, day)
+    data = @form.form_create
+    @user.add_goal(data[:name], data[:expected_amount], data[:year], data[:month], data[:day])
     puts "\nLa meta #{name} ha sido creado con exito!"
   end
 
-  def delete    
-    begin
-      print 'Nombre de la meta a eliminar: '
-    end while !@user_input.validate_user_data_input(field: 'name')
-    name = @user_input.last_input
-    if !@user.search_goal(name).nil?
-      puts "La meta #{name} ha sido cerrada con exito, el dinero esta disponible en la cuenta"
-      @user.search_goal(name).delete(@user.account)
-    else
+  def delete
+    loop do
+      name = @form.form_delete
+      break unless @user.search_goal(name).nil?
       puts "\nLa meta #{name} no existe"
     end
+    @user.search_goal(name).delete(@user.account)
+    puts "La meta #{name} ha sido cerrada con exito, el dinero esta disponible en la cuenta"
   end
 end
--- views/goal_form.rb
+++ views/goal_form.rb
@@ -1,5 +1,57 @@
 class GoalForm
-    def initialize
-        
+  def initialize
+    @user_input = UserInput.new
+  end
+
+  def form_deposit
+    loop do
+      print 'Nombre de la meta: '
+      break unless @user_input.validate_user_data_input(field: 'name')
     end
+    name = @user_input.last_input
+    loop do
+      print 'ingrese la cantidad de dinero que desea agregar a la meta: '
+      break unless @user_input.validate_amount_input
+    end
+    amount = @user_input.last_input.to_i
+    { name: name, amount: amount }
+  end
+
+  def form_create
+    loop do
+      print 'Nombre de la meta: '
+      break unless @user_input.validate_user_data_input(field: 'name')
+    end
+    name = @user_input.last_input
+    loop do
+      print 'Nombre de la meta: '
+      break unless @user_input.validate_amount_input
+    end
+    expected_amount = @user_input.last_input.to_i
+    puts 'Por favor ingrese el año, el mes y el dia de vencimiento de la meta en numeros'
+    loop do
+      print 'Ingrese el plazo maximo de la meta (año): '
+      break unless @user_input.validate_date_input(type: 'year')
+    end
+    year = @user_input.last_input.to_i
+    loop do
+      print 'Ingrese el plazo maximo de la meta (mes): '
+      break unless @user_input.validate_date_input(type: 'month')
+    end
+    month = @user_input.last_input.to_i
+    loop do
+      print 'Ingrese el plazo maximo de la meta (dia): '
+      break unless @user_input.validate_date_input(type: 'day')
+    end
+    day = @user_input.last_input.to_i
+    { name: name, expected_amount: expected_amount, year: year, month: month, day: day }
+  end
+
+  def form_delete
+    loop do
+      print 'Nombre de la meta a eliminar: '
+      break unless @user_input.validate_user_data_input(field: 'name')
+    end
+    @user_input.last_input
+  end
 end
