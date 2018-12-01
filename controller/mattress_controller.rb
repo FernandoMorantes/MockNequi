10a6ab1c6d89c4c7665c983f270ab283c70cc06f
alexis holguin <wholguinmor@uniminuto.edu.co>
2018-12-01 17:53:48 -0500

    controller y form of mattress

--- controller/mattress_controller.rb
+++ controller/mattress_controller.rb
@@ -1,15 +1,11 @@
 class MattressController
-
   def initialize(user:)
     @user_input = UserInput.new
     @user = user
   end
 
   def deposit
-    begin
-      print 'ingrese la cantidad de dinero que desea guardar en el colchon: '
-    end while !@user_input.validate_amount_input
-    amount = @user_input.last_input.to_i
+    amount = @form.form_deposit
     if @user.mattress.deposit(amount, @user.account.available)
       @user.account.available -= amount
       puts "\nDinero depositado con exito!"
@@ -19,10 +15,7 @@ class MattressController
   end
 
   def withdraw
-    begin
-      print 'ingrese la cantidad de dinero que desea retirar del colchon: '
-    end while !@user_input.validate_amount_input
-    amount = @user_input.last_input.to_i.to_i
+    amount = @form.form_withdraw
     if @user.mattress.withdraw(amount)
       @user.account.available += amount
       puts "\nRetiro realizado con exito!"
--- views/mattress_form.rb
+++ views/mattress_form.rb
@@ -1,3 +1,21 @@
 class MattressForm
-  def initialize; end
+  def initialize
+    @user_input = UserInput.new
+  end
+
+  def form_deposit
+    loop do
+      print 'ingrese la cantidad de dinero que desea guardar en el colchon: '
+      break unless @user_input.validate_amount_input
+    end
+    @user_input.last_input.to_i
+  end
+
+  def form_withdraw
+    loop do
+      print 'ingrese la cantidad de dinero que desea retirar del colchon: '
+      break unless @user_input.validate_amount_input
+    end
+    @user_input.last_input.to_i
+  end
 end
